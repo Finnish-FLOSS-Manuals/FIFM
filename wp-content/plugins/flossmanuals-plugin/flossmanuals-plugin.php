6c0f479b60007983b826053a1f975389bf2fc6ad
Tomi Toivio <tomi.toivio@ukuli.fi>
2021-02-03 11:00:52 +0200

    Empty FLOSS Manuals plugin

---
+++ wp-content/plugins/flossmanuals-plugin/flossmanuals-plugin.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name:       FLOSS Manuals Plugin
+ * Plugin URI:        https://fi.flossmanuals.net/
+ * Description:       FLOSS Manuals plugin.
+ * Version:           1.
+ * Author:            Tomi Toivio
+ * Author URI:        https://ukuli.fi/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       flossmanuals-plugin
+ * Domain Path:       /languages
+ */
